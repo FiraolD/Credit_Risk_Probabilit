f518c835c9a444b49c2b9d7ebcc5fb7d15bc894a
FiraolD <firaoldelesa18@gmail.com>
2025-06-30 01:03:30 -0700

    main api updated

--- src/api/main.py
+++ src/api/main.py
@@ -1,18 +1,16 @@
 # src/api/main.py
-
 import pandas as pd
 import joblib
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List, Optional
+from fastapi import HTTPException
 
 app = FastAPI(title="Credit Risk API", description="Predict customer risk using trained model", version="1.0")
 
 # Load best model
 model = joblib.load("models/random_forest_best.pkl")
 print("✅ Model loaded")
-
-
 class CreditRiskRequest(BaseModel):
     TransactionId: str
     BatchId: str
@@ -36,12 +34,9 @@ class CreditRiskRequest(BaseModel):
     MaxTransactionAmount: float
     NumUniqueProducts: int
     NumUniqueChannels: int
-
-
 class CreditRiskResponse(BaseModel):
     is_high_risk: int
     risk_probability: float
-
 
 @app.post("/predict", response_model=CreditRiskResponse)
 def predict(request: CreditRiskRequest):
@@ -55,9 +50,7 @@ def predict(request: CreditRiskRequest):
         # Predict
         proba = model.predict_proba(input_data)[0][1]
         prediction = (proba > 0.5).astype(int)
-        
         print(f"✅ Prediction: {prediction}, Probability: {proba}")
-        
         return {
             "is_high_risk": int(prediction),
             "risk_probability": float(proba)
